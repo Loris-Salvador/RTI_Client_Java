4b8f43f92c66c2453861f0373404978a899a89b5
Loris Salvador <loris.salvador@student.hepl.be>
2023-10-03 11:55:33 +0200

    stp

--- src/main/java/hepl/be/Main.java
+++ src/main/java/hepl/be/Main.java
@@ -6,7 +6,7 @@ import hepl.be.view.window.WindowClient;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("yo");
+        System.out.println("yo1");
         WindowClient mainWindow = new WindowClient();
         MainWindowController mainWindowController = new MainWindowController(mainWindow);
         mainWindow.setController(mainWindowController);
