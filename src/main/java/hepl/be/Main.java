93289237a4788e40cb27466f861547d2698edda4
AntoinePotty <antoine.potty@student.hepl.be>
2023-10-03 11:46:00 +0200

    test

--- src/main/java/hepl/be/Main.java
+++ src/main/java/hepl/be/Main.java
@@ -5,6 +5,8 @@ import hepl.be.view.window.WindowClient;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("yo");
         WindowClient mainWindow = new WindowClient();
         MainWindowController mainWindowController = new MainWindowController(mainWindow);
         mainWindow.setController(mainWindowController);
