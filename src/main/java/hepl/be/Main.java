1a03037b8e80e3695d4289a4348bdc1c30c50477
AntoinePotty <antoine.potty@student.hepl.be>
2023-10-03 13:01:04 +0200

    woo4

--- src/main/java/hepl/be/Main.java
+++ src/main/java/hepl/be/Main.java
@@ -6,7 +6,7 @@ import hepl.be.view.window.WindowClient;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO333333333333333333");
+        System.out.println("WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO444444444444");
         WindowClient mainWindow = new WindowClient();
         MainWindowController mainWindowController = new MainWindowController(mainWindow);
         mainWindow.setController(mainWindowController);
